e5d5663d3800aee30386b10b807550d47ed228fa
hernian14 <hernan.sarmiento@uc.cl>
2023-10-15 20:11:39 -0300

    datos listos

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const SVG2 = d3.select("#vis-2").append("svg");
 
 function parseo(d) {
   return {
-    titulo: d.Titulo,
+    titulo: d["Título"],
     director: d.Director,
-    pais: d.Pais,
+    pais: d["País"],
     estreno_fecha: d["Año estreno (Fecha)"],
     estreno_ano: parseInt(d["Año estreno (Número)"]),
     adicion: d["Fecha adición"],
@@ -17,7 +17,15 @@ function parseo(d) {
   };
 }
 
+function joinDeDatos(datos) {
+    const data2020 = datos.filter((d) => d.estreno_ano >= 2020);
+    console.log(data2020);
+}
+
+
+
+
 d3.csv("Netflix dataset - ConRandoms.csv", parseo)
     .then((datos) => {
-        console.log(datos);
+        joinDeDatos(datos);
         })
